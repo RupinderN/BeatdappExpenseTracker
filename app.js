84f4fa124325960683636cb93bc5e4a45af797d6
Rupinder Nagra <rupinder0410@gmail.com>
2020-04-22 17:46:37 -0400

    added comments & page redirect

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+// ====================================
+// REQUIRE NECESSARY PACKAGES (EXPRESS)
+// ====================================
 const express = require("express"),
-app = express();
+    app = express();
 
+// =================
+// APP CONFIGURATION
+// =================
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// ==================
+// APPLICATION ROUTES
+// ==================
 app.get("/", (req, res) => {
     res.render("main");
 });
 
+app.get("*", (req, res) => {
+    res.redirect("/");
+});
+
+// ==============
+// SERVER STARTUP
+// ==============
 app.listen(3000, () => {
     console.log("Server is running!");
 });
